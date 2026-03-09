838627454db88fd1e09ecfc7478c4be3d32e1363
Sana Deghat <Sana.Deghat@gmail.com>
2026-03-09 01:36:37 -0700

    animation
    added loading animatio nyippeee

--- spotlight.js
+++ spotlight.js
@@ -1,23 +1,25 @@
-// taken from https://github.com/hackclub/athena/blob/main/components/Background.tsx (adapted to be not-React)
-const spotlightOverlay = document.getElementById('spotlight-overlay');
+// Teleport the spotlight out of the main-content cage so animations don't break it!
 const spotlight = document.getElementById('spotlight');
+document.body.prepend(spotlight); 
+
+const spotlightOverlay = document.getElementById('spotlight-overlay');
 let overlayShown = false;
 
 window.addEventListener('mousemove', (event) => {
-const rect = spotlight.getBoundingClientRect();
-const x = event.clientX - rect.left;
-const y = event.clientY - rect.top;
+    const rect = spotlight.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
-if (!overlayShown) {
-    spotlight.style.opacity = '1';
-    overlayShown = true;
-}
+    if (!overlayShown) {
+        spotlight.style.opacity = '1';
+        overlayShown = true;
+    }
 
-spotlightOverlay.style.background = `
-    radial-gradient(
-    circle at ${x}px ${y}px,
-    rgba(132, 146, 166, 0) 10px,
-    rgba(249, 250, 252, 0.8) 125px
-    )
-`;
+    spotlightOverlay.style.background = `
+        radial-gradient(
+        circle at ${x}px ${y}px,
+        rgba(132, 146, 166, 0) 10px,
+        rgba(249, 250, 252, 0.8) 125px
+        )
+    `;
 });
